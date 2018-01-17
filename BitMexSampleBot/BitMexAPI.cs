c3671c6a10c0a0b9165b4a119ae2495fe8966db8
BigBitsCode <BigBitsCode@gmail.com>
2018-01-17 12:47:11 -0500

    again

--- BitMexSampleBot/BitMexAPI.cs
+++ BitMexSampleBot/BitMexAPI.cs
@@ -333,7 +333,6 @@ namespace BitMEX
         public double? BBUpper { get; set; }
         public double? BBMiddle { get; set; }
         public double? BBLower { get; set; }
-        public double? BBMultiplier { get; set; }
     }
 
     public class Position
