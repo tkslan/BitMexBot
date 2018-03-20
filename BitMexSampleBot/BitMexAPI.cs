4ab406f06c572517a0f96721308f64756c42f033
BigBitsCode <BigBitsCode@gmail.com>
2018-03-19 20:40:32 -0400

    Fixed STOCH

--- BitMexSampleBot/BitMexAPI.cs
+++ BitMexSampleBot/BitMexAPI.cs
@@ -379,8 +379,8 @@ namespace BitMEX
         public int PCC { get; set; }
         public double? MA1 { get; set; }
         public double? MA2 { get; set; }
-        public double? STOCHK;
-        public double? STOCHD;
+        public double? STOCHK { get; set; }
+        public double? STOCHD { get; set; }
         public double? BBUpper { get; set; }
         public double? BBMiddle { get; set; }
         public double? BBLower { get; set; }
